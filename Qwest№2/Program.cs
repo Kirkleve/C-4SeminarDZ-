f61a7de21026fffe88be388eda01cc40dd91ccd9
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-26 00:02:31 +0200

    Не решена 3 задачa

--- Qwest№2/Program.cs
+++ Qwest№2/Program.cs
@@ -3,17 +3,6 @@
 82 -> 10
 9012 -> 12*/
 
-/*Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int numSum = 0;
-
-while (number !=0)
-{
-    numSum = numSum + number % 10;
-    number = number / 10;
-}
-Console.WriteLine(numSum);*/
-
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int numSum = 0;
--- Qwest№3/Program.cs
+++ Qwest№3/Program.cs
@@ -1,7 +1,24 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
-Поправка: на семинаре дал добро на заволнение массива рандомными числами. 
+Поправка: на семинаре дал добро на заполнение массива рандомными числами. 
 В оригинале нужно пользователю ввести строку - ряд чисел через запятую,
 а программа превращает эту строку в полноценный массив и выводит. Рекомендую вторым вариантом)*/
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите длину массива из 8 элементов ");
+int[] array = new int[8];
+string text = String.Empty;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    text = text + array[i] + ", ";
+}
+
+char [] massive = new char [text.Length];
+    for(int i = 0; i < text.Length; i++)
+    {
+        array[i] = text[i];
+    }
+Console.WriteLine(text);
+Console.WriteLine();
