6f22541d6b3e2d260c3ae711add92a618e798fb3
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-29 22:13:32 +0200

    3 задача так и не побеждена(

--- Qwest3(1)/Program.cs
+++ Qwest3(1)/Program.cs
@@ -1,4 +1,39 @@
-﻿Console.WriteLine("Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+﻿//Это третья задача которую так и не решил, суть такова, пытался сделать так 
+//чтобы пользователь вводил любые числа, потом это всё переводилось строкой через запятую, ну а потом в массив!
+// вывести в массив так и не получилось( как только не пробовал.
+
+
+
+Console.WriteLine("Введите длину массива из 8 элементов ");
+int[] array = new int[8];
+string text = String.Empty;
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    text = text + array[i] + ","+" ";
+}
+
+Console.WriteLine(text);
+
+for (int i = 0; i < array.Length; i++)
+{
+    string[] _arrayNumbers = text.Split(new char[] {','});
+    text = text + array[i];
+}
+
+Console.WriteLine($"Длинна массива из 8 чисел = " + text);
+
+/*char [] massive = new char [text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+        array[i] = text[i];
+    }
+Console.WriteLine($"{text}");*/
+
+
+/*Console.WriteLine("Введите 8 чисел");
 int[] array = new int[8];
 FillArray(array);
 Console.WriteLine($"Длинна массива {array.Length}");
@@ -20,4 +55,4 @@ void FillArray(int[] collection)
         collection[index] = Convert.ToInt32(Console.ReadLine());
         index++;
     }
-} 
+} */
--- Qwest№3/Program.cs
+++ Qwest№3/Program.cs
@@ -5,64 +5,13 @@
 В оригинале нужно пользователю ввести строку - ряд чисел через запятую,
 а программа превращает эту строку в полноценный массив и выводит. Рекомендую вторым вариантом)*/
 
-/*Console.WriteLine("Введите длину массива из 8 элементов ");
-int[] array = new int[8];
-string text = String.Empty;
+Console.WriteLine("Введите значения массива через запятую");
 
-for (int i = 0; i < array.Length; i++)
+string array = Console.ReadLine();
+
+int index = 0;
+while(index<array.Length)
 {
-    
-    array[i] = Convert.ToInt32(Console.ReadLine());
-    text = text + array[i] + ",";
+    Console.Write( array[index]+ " ");
+    index+=2 ;
 }
-
-Console.WriteLine(text);
-for (int i = 0; i < array.Length; i++)
-{
-    string[] _arrayNumbers = text.Split(new char[] {','});
-    text = text + array[i];
-}
-Console.WriteLine(text);*/
-
-
-
-/*char [] massive = new char [text.Length];
-    for (int i = 0; i < text.Length; i++)
-    {
-        array[i] = text[i];
-    }
-Console.WriteLine($"{text}");
-Console.WriteLine();*/
-
-Console.Write("Введите числа через запятую: ");
-string[] array = new string[8];
-string numbers = String.Empty;
-
-IntString(numbers);
-
-void IntString (string num)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-        string array = Console.ReadLine();
-        numbers = numbers + array[i] + ",";
-        Console.Write(numbers);
-    }
-}
-Console.Write(numbers);
-void Simbol(string str)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        
-    }
-}
-
-
-/*numbers = numbers.Replace(" ", "");
-string[] _arrayNumbers = numbers.Split(new char[] {','});
-
-for(int i = 0; i < _arrayNumbers.Length; i++)
-{
-    Console.Write($"{_arrayNumbers[i]} ");
-}*/
